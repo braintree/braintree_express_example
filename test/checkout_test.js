47a04431d75bc4d4f8d6ff5e003ba6414d080276
jerry and katy <code@getbraintree.com>
2015-11-10 20:32:38 +0000

    Remove should from test descriptions

--- test/checkout_test.js
+++ test/checkout_test.js
@@ -3,25 +3,25 @@ var supertest = require('supertest');
 var api = supertest("http://localhost:3000");
 
 describe('Checkout index page', function(){
-  it("should respond with 200", function(done){
+  it("responds with 200", function(done){
     api.get("/checkouts/new").expect(200, done);
   });
 
-  it("should generate a client token", function(done){
+  it("generates a client token", function(done){
     api.get("/checkouts/new").end(function(err, res) {
       expect(res.text).to.match(/var token = \"[\w=]+\";/);
       done();
     });
   });
 
-  it("should include the checkout form", function(done){
+  it("includes the checkout form", function(done){
     api.get("/checkouts/new").end(function(err, res) {
       expect(res.text).to.match(/<form id="checkout"/);
       done();
     });
   });
 
-  it("should include the dropin payment-form div", function(done){
+  it("includes the dropin payment-form div", function(done){
     api.get("/checkouts/new").end(function(err, res) {
       expect(res.text).to.match(/<div id="payment-form"/);
       done();
