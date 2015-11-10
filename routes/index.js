bb7eb52313831da6a8c12324d220875fd25f68bf
jerry and katy <code@getbraintree.com>
2015-11-10 15:43:07 +0000

    Update root route to point to new checkouts page

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
 router.get('/checkouts/new', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
