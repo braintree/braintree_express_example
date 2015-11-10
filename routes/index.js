60fba961804cecfcd45be6a8b64ccbd0d2102300
jerry and katy <code@getbraintree.com>
2015-11-10 19:47:21 +0000

    Add Braintree client token to checkout page

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var gateway = require('../lib/gateway');
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.get('/checkouts/new', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var checkoutsController = this;
+
+  gateway.clientToken.generate({}, function(err, response){
+    checkoutsController.clientToken = response.clientToken;
+  });
+
+  res.render('index', { title: 'Express', clientToken: checkoutsController.clientToken});
 });
 
 module.exports = router;
--- test/checkout_test.js
+++ test/checkout_test.js
@@ -6,4 +6,11 @@ describe('Checkout index page', function(){
   it("should respond with 200", function(done){
     api.get("/checkouts/new").expect(200, done);
   });
+
+  it("should generate a client token", function(done){
+    api.get("/checkouts/new").end(function(err, res) {
+      expect(res.text).to.match(/var token = \"[\w=]+\";/);
+      done();
+    });
+  });
 });
