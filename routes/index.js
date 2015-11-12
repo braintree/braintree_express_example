679d255dafe18370457df7425a25df1b6892fe48
jerry and katy <code@getbraintree.com>
2015-11-12 22:57:53 +0000

    Handle processor and gateway rejection error responses

--- routes/index.js
+++ routes/index.js
@@ -24,7 +24,7 @@ router.get('/checkouts/:id', function(req, res, next){
   transaction_id = req.params.id;
 
   gateway.transaction.find(transaction_id, function(err, transaction){
-    res.render('checkouts/show', {transaction: transaction});
+    res.render('checkouts/show', {transaction: transaction, messages: req.flash('error')});
   });
 });
 
@@ -37,6 +37,9 @@ router.post('/checkouts', function(req, res, next){
     paymentMethodNonce: nonce,
   }, function(err, result){
     if (result.success) {
+      res.redirect('checkouts/' + result.transaction.id)
+    } else if (result.transaction) {
+      req.flash('error', { msg: 'Transaction status - ' + result.transaction.status });
       res.redirect('checkouts/' + result.transaction.id)
     } else {
       var transactionErrors = result.errors.deepErrors();
--- test/checkout_test.js
+++ test/checkout_test.js
@@ -86,26 +86,53 @@ describe("Checkouts create", function(){
   });
 
   describe("when the transaction is not successful", function(){
-    it("redirects to the new checkout page if transaction is not created", function(done) {
-      api.post("/checkouts")
+    describe("when braintree returns an error", function(){
+      it("redirects to the new checkout page if transaction is not created", function(done) {
+        api.post("/checkouts")
         .send({amount: 'not_a_valid_amount', payment_method_nonce: 'not_a_valid_nonce'})
         .expect(302)
         .expect('Location', 'checkouts/new')
         .end(done);
+      });
+
+      it("displays errors", function(done) {
+        api.post("/checkouts")
+          .send({amount: 'not_a_valid_amount', payment_method_nonce: 'not_a_valid_nonce'})
+          .end(function(err, res){
+            agent.saveCookies(res);
+            var req = api.get("/checkouts/new")
+            agent.attachCookies(req);
+            req.end(function (err, res){
+              expect(res.text).to.contain("Amount is an invalid format");
+              done();
+            });
+          });
+      });
     });
 
-    it("displays errors", function(done) {
-      api.post("/checkouts")
-        .send({amount: 'not_a_valid_amount', payment_method_nonce: 'not_a_valid_nonce'})
-        .end(function(err, res){
-          agent.saveCookies(res);
-          var req = api.get("/checkouts/new")
-          agent.attachCookies(req);
-          req.end(function (err, res){
-            expect(res.text).to.contain("Amount is an invalid format");
-            done();
+    describe("when there are processor errors", function(){
+      it("redirects to the checkouts show page", function(done) {
+        api.post("/checkouts")
+        .send({amount: '2000.00', payment_method_nonce: 'fake-valid-nonce'})
+        .expect(302)
+        .expect('Location', /checkouts\/[^new$][\w+]/)
+        .end(done);
+      });
+
+      it("displays the transaction status", function(done) {
+        api.post("/checkouts")
+          .send({amount: '2000.00', payment_method_nonce: 'fake-valid-nonce'})
+          .end(function(err, res){
+            agent.saveCookies(res);
+            redirectUrl = "/" + res.req.res.headers['location']
+            var req = api.get(redirectUrl)
+            agent.attachCookies(req);
+            req.end(function (err, res){
+              expect(res.text).to.contain("Transaction status - processor_declined");
+              done();
+            });
           });
-        });
+      });
     });
   });
 });
