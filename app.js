53fa6a526658e6817cd46d1664ea35b853a95460
Evan Hahn <me@evanhahn.com>
2015-11-19 15:18:20 -0800

    Lint the code with ESLint

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+'use strict';
+
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -16,10 +17,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(session({
   // this string is not an appropriate value for a production environment
@@ -34,9 +35,11 @@ app.use(flash());
 app.use('/', routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
+
   err.status = 404;
+
   next(err);
 });
 
@@ -45,7 +48,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) { // eslint-disable-line no-unused-vars
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -56,13 +59,12 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) { // eslint-disable-line no-unused-vars
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
     error: {}
   });
 });
-
 
 module.exports = app;
--- test/checkout_test.js
+++ test/checkout_test.js
@@ -1,134 +1,150 @@
+'use strict';
+
 var expect = require('chai').expect;
 var supertest = require('supertest');
 var superagent = require('superagent');
 var agent = superagent.agent();
 
-var api = supertest("http://localhost:3000");
+var api = supertest('http://localhost:3000');
 var gateway = require('../lib/gateway');
 
-describe('Checkout index page', function(){
-  it("redirects to the checkouts new page", function(done){
-    api.get("/").expect(302, done);
+describe('Checkout index page', function () {
+  it('redirects to the checkouts new page', function (done) {
+    api.get('/').expect(302, done);
   });
 
-  it("responds with 200", function(done){
-    api.get("/checkouts/new").expect(200, done);
+  it('responds with 200', function (done) {
+    api.get('/checkouts/new').expect(200, done);
   });
 
-  it("generates a client token", function(done){
-    api.get("/checkouts/new").end(function(err, res) {
-      expect(res.text).to.match(/var token = \"[\w=]+\";/);
+  it('generates a client token', function (done) {
+    api.get('/checkouts/new').end(function (err, res) {
+      expect(res.text).to.match(/var token = \'[\w=]+\';/);
       done();
     });
   });
 
-  it("includes the checkout form", function(done){
-    api.get("/checkouts/new").end(function(err, res) {
-      expect(res.text).to.match(/<form id="checkout"/);
+  it('includes the checkout form', function (done) {
+    api.get('/checkouts/new').end(function (err, res) {
+      expect(res.text).to.match(/<form id='checkout'/);
       done();
     });
   });
 
-  it("includes the dropin payment-form div", function(done){
-    api.get("/checkouts/new").end(function(err, res) {
-      expect(res.text).to.match(/<div id="payment-form"/);
+  it('includes the dropin payment-form div', function (done) {
+    api.get('/checkouts/new').end(function (err, res) {
+      expect(res.text).to.match(/<div id='payment-form'/);
       done();
     });
   });
 
-  it("includes the amount field", function(done){
-    api.get("/checkouts/new").end(function(err, res) {
-      expect(res.text).to.match(/<label for="amount/);
-      expect(res.text).to.match(/<input type="text" name="amount" id="amount/);
+  it('includes the amount field', function (done) {
+    api.get('/checkouts/new').end(function (err, res) {
+      expect(res.text).to.match(/<label for='amount/);
+      expect(res.text).to.match(/<input type='text' name='amount' id='amount/);
       done();
     });
   });
 });
 
-describe("Checkouts show page", function(){
-  it("respond with 200", function(done){
+describe('Checkouts show page', function () {
+  it('respond with 200', function (done) {
     gateway.transaction.sale({
-      amount: "10.00",
-      paymentMethodNonce: "fake-valid-nonce"
-    }, function(err, result){
-      transaction = result.transaction
-      api.get("/checkouts/" + transaction.id).expect(200, done);
+      amount: '10.00',
+      paymentMethodNonce: 'fake-valid-nonce'
+    }, function (err, result) {
+      var transaction = result.transaction;
+
+      api.get('/checkouts/' + transaction.id).expect(200, done);
     });
   });
 
-  it("displays the transaction's fields", function(done){
+  it("displays the transaction's fields", function (done) {
     gateway.transaction.sale({
-      amount: "10.00",
-      paymentMethodNonce: "fake-valid-nonce"
-    }, function(err, result){
-      transaction = result.transaction
-      api.get("/checkouts/" + transaction.id).end(function(err, res){
-        expect(res.text).to.contain(transaction.id)
-        expect(res.text).to.contain(transaction.type)
-        expect(res.text).to.contain(transaction.amount)
-        expect(res.text).to.contain(transaction.status)
-        expect(res.text).to.contain(transaction.creditCard.bin)
-        expect(res.text).to.contain(transaction.creditCard.last4)
-        expect(res.text).to.contain(transaction.creditCard.cardType)
-        expect(res.text).to.contain(transaction.creditCard.expirationDate)
-        expect(res.text).to.contain(transaction.creditCard.customerLocation)
+      amount: '10.00',
+      paymentMethodNonce: 'fake-valid-nonce'
+    }, function (err, result) {
+      var transaction = result.transaction;
+
+      api.get('/checkouts/' + transaction.id).end(function (err, res) {
+        expect(res.text).to.contain(transaction.id);
+        expect(res.text).to.contain(transaction.type);
+        expect(res.text).to.contain(transaction.amount);
+        expect(res.text).to.contain(transaction.status);
+        expect(res.text).to.contain(transaction.creditCard.bin);
+        expect(res.text).to.contain(transaction.creditCard.last4);
+        expect(res.text).to.contain(transaction.creditCard.cardType);
+        expect(res.text).to.contain(transaction.creditCard.expirationDate);
+        expect(res.text).to.contain(transaction.creditCard.customerLocation);
         done();
       });
     });
   });
 });
 
-describe("Checkouts create", function(){
-  it("creates a transaction and redirects to checkout show", function(done){
-    api.post("/checkouts")
-      .send({amount: '10.00', payment_method_nonce: 'fake-valid-nonce'})
-      .expect(302, done)
+describe('Checkouts create', function () {
+  it('creates a transaction and redirects to checkout show', function (done) {
+    api.post('/checkouts')
+    .send({
+      amount: '10.00',
+      payment_method_nonce: 'fake-valid-nonce'
+    })
+    .expect(302, done);
   });
 
-  context("when the transaction is not successful", function(){
-    context("when braintree returns an error", function(){
-      it("redirects to the new checkout page if transaction is not created", function(done) {
-        api.post("/checkouts")
-        .send({amount: 'not_a_valid_amount', payment_method_nonce: 'not_a_valid_nonce'})
+  context('when the transaction is not successful', function () {
+    context('when braintree returns an error', function () {
+      it('redirects to the new checkout page if transaction is not created', function (done) {
+        api.post('/checkouts')
+        .send({
+          amount: 'not_a_valid_amount',
+          payment_method_nonce: 'not_a_valid_nonce'
+        })
         .expect(302)
         .expect('Location', 'checkouts/new')
         .end(done);
       });
 
-      it("displays errors", function(done) {
-        api.post("/checkouts")
+      it('displays errors', function (done) {
+        api.post('/checkouts')
           .send({amount: 'not_a_valid_amount', payment_method_nonce: 'not_a_valid_nonce'})
-          .end(function(err, res){
+          .end(function (err, res) {
+            var req;
+
             agent.saveCookies(res);
-            var req = api.get("/checkouts/new")
+            req = api.get('/checkouts/new');
+
             agent.attachCookies(req);
-            req.end(function (err, res){
-              expect(res.text).to.contain("Amount is an invalid format");
+
+            req.end(function (err, res) {
+              expect(res.text).to.contain('Amount is an invalid format');
               done();
             });
           });
       });
     });
 
-    context("when there are processor errors", function(){
-      it("redirects to the checkouts show page", function(done) {
-        api.post("/checkouts")
+    context('when there are processor errors', function () {
+      it('redirects to the checkouts show page', function (done) {
+        api.post('/checkouts')
         .send({amount: '2000.00', payment_method_nonce: 'fake-valid-nonce'})
         .expect(302)
         .expect('Location', /checkouts\/[^new$][\w+]/)
         .end(done);
       });
 
-      it("displays the transaction status", function(done) {
-        api.post("/checkouts")
+      it('displays the transaction status', function (done) {
+        api.post('/checkouts')
           .send({amount: '2000.00', payment_method_nonce: 'fake-valid-nonce'})
-          .end(function(err, res){
+          .end(function (err, res) {
+            var redirectUrl, req;
+
             agent.saveCookies(res);
-            redirectUrl = "/" + res.req.res.headers['location']
-            var req = api.get(redirectUrl)
+            redirectUrl = '/' + res.headers.location;
+            req = api.get(redirectUrl);
             agent.attachCookies(req);
-            req.end(function (err, res){
-              expect(res.text).to.contain("Transaction status - processor_declined");
+            req.end(function (err, res) {
+              expect(res.text).to.contain('Transaction status - processor_declined');
               done();
             });
           });
