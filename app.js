b4776b9a9087a87f88a9c12816c67e4d3efe8c8e
Blade Barringer <blade.barringer@getbraintree.com>
2019-01-23 21:12:15 +0000

    Update jade -> pug

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(favicon(path.join(__dirname, 'public/images', 'favicon.png')));
 app.use(logger('dev'));
