2a44fc5173fbbc6cc799debf72d64fb365a228ac
Blade Barringer <blade.barringer@getbraintree.com>
2019-06-05 21:43:50 +0000

    Update dependencies

--- test/checkout_test.js
+++ test/checkout_test.js
@@ -136,10 +136,10 @@ describe('Checkouts create', function () {
     context('when braintree returns an error', function () {
       it('redirects to the new checkout page if transaction is not created', function (done) {
         api.post('/checkouts')
-        .send({amount: 'not_a_valid_amount', payment_method_nonce: 'not_a_valid_nonce'}) // eslint-disable-line camelcase
-        .expect(302)
-        .expect('Location', 'checkouts/new')
-        .end(done);
+          .send({amount: 'not_a_valid_amount', payment_method_nonce: 'not_a_valid_nonce'}) // eslint-disable-line camelcase
+          .expect(302)
+          .expect('Location', 'checkouts/new')
+          .end(done);
       });
 
       it('displays errors', function (done) {
@@ -162,10 +162,10 @@ describe('Checkouts create', function () {
     context('when there are processor errors', function () {
       it('redirects to the checkouts show page', function (done) {
         api.post('/checkouts')
-        .send({amount: '2000.00', payment_method_nonce: 'fake-valid-nonce'}) // eslint-disable-line camelcase
-        .expect(302)
-        .expect('Location', /checkouts\/[^new$][\w+]/)
-        .end(done);
+          .send({amount: '2000.00', payment_method_nonce: 'fake-valid-nonce'}) // eslint-disable-line camelcase
+          .expect(302)
+          .expect('Location', /checkouts\/[^new$][\w+]/)
+          .end(done);
       });
 
       it('displays the transaction status', function (done) {
