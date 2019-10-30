17ddd338457729e442d9947b2c1e64aaf9f5886d
aki 🌹 <aki.braun@getbraintree.com>
2019-10-29 21:52:59 -0700

    how did i break the build tho that was so innocent

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  mode:
-    process.env.npm_lifecycle_event === 'start' ? 'production' : 'development',
+  mode: 'development',
   entry: './src/javascript/demo.js',
   output: {
     path: path.resolve(__dirname, 'public', 'javascript'),
